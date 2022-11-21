be467c47d5b16dcbc9c6a0a2a381fb04aec0d2b3
VictorPoquet <victorpoquetjob@gmail.com>
2022-11-21 09:53:42 +0100

    Ejercicio hospital

--- main.py
+++ main.py
@@ -1,17 +1,76 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import hospital_clases
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MetodosCreate:
+    num_hos = 0
+    num_doctor = 0
+    num_enfer = 0
+
+    def __init__(self, hospital_clas):
+        self.hs = hospital_clas
+
+    # CREA 2 DOCTORES
+    def crear_doctores(self):
+        doctores = []
+        for num_doc in range(2):
+            print("vuelta ", num_doc)
+            self.num_doctor += 1
+            nombre_d = "DOC" + str(self.num_doctor)
+            apellido_d = nombre_d + " " + nombre_d
+            dni_d = str(self.num_doctor) + "dni"
+            especialidades_list = ["Alergología", "Anestesiología y reanimación", "Aparato digestivo", "Cardiología"]
+            especialidad = random.choice(especialidades_list)
+            print(especialidad)
+            doctores = hs.Doctor(nombre_d, apellido_d, dni_d, especialidad)
+        return doctores
+
+    # CREA 2 ENFERMEROS
+    def crear_enfermero(self):
+        list_enfermeros = []
+        for num_enf in range(2):
+            print("vuelta ", num_enf)
+            self.num_enfer += 1
+            nombre_e = "Enf" + str(self.num_enfer)
+            apellido_e = nombre_e + " " + nombre_e
+            dni_e = str(self.num_enfer) + "dnienfermero"
+
+            plantas_list = [1, 2, 3, 4, 5]
+            planta = random.choice(plantas_list)
+            print("trabaja en la planta", planta)
+            list_enfermeros = self.hs.Enfermeros(nombre_e, apellido_e, dni_e, planta, False)
+        return list_enfermeros
+
+    # CREA Hospital
+    def crear_hospital(self):
+        doctores = self.crear_doctores()
+        enfermeros = self.crear_enfermero()
+        self.num_hos += 1
+        nombre_h = "h" + str(self.num_hos)
+        ubicacion_h = "u" + str(self.num_hos)
+        hospital = hs.Hospital(nombre_h, ubicacion_h, doctores, enfermeros, enfermos=[])
+        return hospital
+
+    # IMPRIME LO QUE HAY EN LAS SALAS DE ESPERA
+    def motrar_sala_espera(self, sala):
+        print("EN LA SALA DE ESPERA HAY:")
+        for p in sala:
+            print("\t- ", p.nombre)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Victor  Poquet')
+sala_de_espera = []
+hs = hospital_clases
 
+mCreate = MetodosCreate(hs)
+# Crear 4 Pacientes y meterlos en la sala de espera
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+p1 = hs.Paciente("Jose", "Jose Jose", "1111A", "", False)
+p2 = hs.Paciente("Maria", "Maria Maria", "2222B", "", False)
+p3 = hs.Paciente("Pepa", "Pepa Pepa", "3333C", "", False)
+p4 = hs.Paciente("Antonio", "Antonio Antonio", "4444D", "", False)
+
+sala_de_espera = [p1, p2, p3, p4]
+mCreate.motrar_sala_espera(sala_de_espera)
+
+# Creamos hospital y sus trabajadores
+hopital1 = mCreate.crear_hospital()
